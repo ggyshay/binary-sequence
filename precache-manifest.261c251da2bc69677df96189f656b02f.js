66404c797d240b3324bd7d729d8846da60b4f419
yshay <yshay@cuidas.com.br>
2020-04-21 15:09:03 -0300

    Updates

---
+++ precache-manifest.261c251da2bc69677df96189f656b02f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32156ad0d37300e1e358c50654c73554",
+    "url": "/binary-sequence/index.html"
+  },
+  {
+    "revision": "b1b3f482cb8a1ff1a4aa",
+    "url": "/binary-sequence/static/css/main.b7c1df1b.chunk.css"
+  },
+  {
+    "revision": "198b5a891e652cc04046",
+    "url": "/binary-sequence/static/js/2.54ad2d94.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/binary-sequence/static/js/2.54ad2d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae6837d34ce5c7c2a12",
+    "url": "/binary-sequence/static/js/3.3ac354d3.chunk.js"
+  },
+  {
+    "revision": "11632a58e510552bf3ae8fe3d05874e6",
+    "url": "/binary-sequence/static/js/3.3ac354d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b3f482cb8a1ff1a4aa",
+    "url": "/binary-sequence/static/js/main.33ca1fb6.chunk.js"
+  },
+  {
+    "revision": "9306a1e872ecc39e3462",
+    "url": "/binary-sequence/static/js/runtime-main.45265c3b.js"
+  },
+  {
+    "revision": "4c4a308786e8df9f434697f7db574327",
+    "url": "/binary-sequence/static/media/alert.4c4a3087.wav"
+  }
+]);
